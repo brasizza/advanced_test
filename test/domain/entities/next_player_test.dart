713aca428305e21d250a71caab17b5f708e29b47
Marcus Brasizza <marcus.brasizza@epoc.tech>
2025-02-07 23:10:22 -0300

    mudando para factory para nao refazer a regra em toda a chamada

--- test/domain/entities/next_player_test.dart
+++ test/domain/entities/next_player_test.dart
@@ -2,22 +2,42 @@ import 'package:flutter_test/flutter_test.dart';
 
 class NexEventPlayer {
   final String id;
+  final String initials;
   final String name;
   final String? photo;
   final String? position;
   final bool isConfirmed;
   final DateTime? confirmationData;
 
-  NexEventPlayer({
+  NexEventPlayer._({
     required this.id,
     required this.name,
+    required this.initials,
     this.photo,
     this.position,
     required this.isConfirmed,
     this.confirmationData,
   });
 
-  String get initials {
+  factory NexEventPlayer({
+    required String id,
+    required String name,
+    String? photo,
+    String? position,
+    required bool isConfirmed,
+    DateTime? confirmationData,
+  }) =>
+      NexEventPlayer._(
+        id: id,
+        name: name,
+        initials: _getInitials(name),
+        photo: photo,
+        position: position,
+        isConfirmed: isConfirmed,
+        confirmationData: confirmationData,
+      );
+
+  static String _getInitials(String name) {
     final names = name.split(' ');
     final firstChar = names.first[0];
     final lastChar = names.last[0];
